1118a0488618167b04d97634d7c8724155c6f2d0
Dhomyav <90100392+Dhomyav@users.noreply.github.com>
2021-10-20 18:00:36 +0530

    Add files via upload

---
+++ js/algorithm.js
@@ -0,0 +1,36 @@
+/* js for navbar */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    var y = document.getElementById("box1");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    }
+   
+    else {
+      x.className = "topnav";
+    }
+  }
+
+
+/* algorithm page js */
+setInterval(() => {
+    let monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+    const c=new Date();
+    const m=monthNames[c.getMonth()];
+    const d=c.getDate();
+    document.getElementById('date').innerHTML=`${m} ${d}`
+  }, 1000);
+  // starting of web quote
+  fetch('https://api.quotable.io/random?maxLength=50')
+  .then(response => response.json())
+  .then(data => {
+  quote1.innerHTML = `"${data.content} —${data.author}"` })
+  // for-loop
+  setInterval(() => {
+  fetch('https://api.quotable.io/random?maxLength=50')
+  .then(response => response.json())
+  .then(data => {
+    quote1.innerHTML = `"${data.content}"" —${data.author}`
+  })
+  },60*1000*60);
